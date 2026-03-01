0ddf89baf18ddf501bccbaacd1fef83959c609a3
peter england <englandpeter998@gmail.com>
2026-03-01 06:43:54 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5094437331-6138e",
+  "appId": "1:519980934711:web:47450f461da765679d01b2",
+  "apiKey": "AIzaSyB7y-gASKEZTKVeSJq10SDHKE6fpswVAVQ",
+  "authDomain": "studio-5094437331-6138e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "519980934711"
+};
